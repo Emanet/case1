316d593c6df284cfbcfb5f26e2f3849bbc49e80d
Emanet <mehmetcanemanet@gmail.com>
2023-11-06 14:41:01 +0100

    feat: error handling for api key exist or nor

--- src/api/services/Status.js
+++ src/api/services/Status.js
@@ -1,8 +1,16 @@
 import client from "../axios";
 
 const FetchStatus = async () => {
-    const { data } = await client.get("recruitment")
-    return data;
+    if (!process.env.NEXT_PUBLIC_API_KEY) {
+        console.warn("API key is missing. Please provide an API key.");
+        return null;
+    }
+    try {
+        const { data } = await client.get("recruitment");
+        return data;
+    } catch (error) {
+        console.error("Error while fetching data:", error);
+        throw error;
+    }
 };
-
 export { FetchStatus };
