5fe09e223359d3b6f4f555021afbfd1c00af8812
Emanet <mehmetcanemanet@gmail.com>
2023-11-08 01:11:51 +0100

    fix: error handling in status

--- src/api/services/Status.js
+++ src/api/services/Status.js
@@ -5,8 +5,13 @@ const FetchStatus = async () => {
         console.warn("API key is missing. Please provide an API key and refresh.");
         return null;
     }
-    const { data } = await client.get("recruitment");
-    return data;
+    try {
+        const response = await client.get("recruitment");
+        console.log('Success Response:', response.data);
+        return response.data;
+    } catch (error) {
+        console.log("Error:", error);
+        return null;
+    }
 }
-
-export { FetchStatus }
+export { FetchStatus };
